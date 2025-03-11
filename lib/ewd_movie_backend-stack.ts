d3492790cc6f97b98f65d636afa3633e48157e3d
Israr Ahmed <israrahmedpk1999@gmail.com>
2025-03-11 14:05:34 +0000

    Initial lambda, API gateway setup , and testing

--- lib/ewd_movie_backend-stack.ts
+++ lib/ewd_movie_backend-stack.ts
@@ -1,16 +1,38 @@
+// Filename: /lib/ewd_movie_backend-stack.ts
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as path from 'path';
 
 export class EwdMovieBackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // CREATE THE LAMBDA FUNCTION FOR API REQUESTS
+    const apiLambda = new lambda.Function(this, 'ApiLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X, // NODE.JS 20y
+      //  RUNTIME
+      handler: 'index.handler', // ENTRY POINT FUNCTION
+      code: lambda.Code.fromAsset(path.join(__dirname, '../dist')), // SOURCE CODE DIRECTORY
+      environment: {
+        TABLE_NAME: 'MovieReviews', // DYNAMODB TABLE NAME
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EwdMovieBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // GRANT LAMBDA PERMISSIONS TO ACCESS DYNAMODB
+    apiLambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['dynamodb:*'],
+        resources: ['*'], // RESTRICT THIS TO THE SPECIFIC TABLE LATER
+      })
+    );
+
+    // SET UP API GATEWAY TO ROUTE REQUESTS TO LAMBDA
+    new apigateway.LambdaRestApi(this, 'MovieReviewAPI', {
+      handler: apiLambda, // CONNECT API GATEWAY TO LAMBDA FUNCTION
+      proxy: true, // ALLOW ALL ROUTES TO BE HANDLED BY LAMBDA
+    });
   }
 }
